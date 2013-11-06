5bf76a31fd6be541174b6a93de813a615f926bef
KCJWagner <kcjwagner@gmail.com>
2013-11-05 16:27:11 -0800

    Added extravagent blue felt background

--- android/gen/com/example/supersmashpoker/R.java
+++ android/gen/com/example/supersmashpoker/R.java
@@ -25,9 +25,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int ssp_play_button=0x7f020001;
-        public static final int ssp_title_bg=0x7f020002;
-        public static final int ssp_title_logo=0x7f020003;
+        public static final int ssp_felt_bg=0x7f020001;
+        public static final int ssp_play_button=0x7f020002;
+        public static final int ssp_title_bg=0x7f020003;
+        public static final int ssp_title_logo=0x7f020004;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
