2e54d8aa1ea7d331a5114a0c548cbc6624dd2657
KCJWagner <kcjwagner@gmail.com>
2013-11-10 13:12:57 -0800

    Seekbar now adjustes the text next to it, and gave buttons names

--- android/gen/com/example/supersmashpoker/R.java
+++ android/gen/com/example/supersmashpoker/R.java
@@ -84,22 +84,24 @@ public final class R {
         public static final int ssp_title_logo=0x7f020039;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f09000d;
+        public static final int LinearLayout1=0x7f09000f;
         public static final int LinearLayout2=0x7f090000;
-        public static final int action_settings=0x7f09000f;
-        public static final int button1=0x7f090005;
-        public static final int button2=0x7f090006;
-        public static final int button3=0x7f090007;
-        public static final int editText1=0x7f090004;
-        public static final int imageView1=0x7f090001;
-        public static final int imageView2=0x7f090002;
-        public static final int ip1=0x7f090008;
-        public static final int ip2=0x7f090009;
-        public static final int ip3=0x7f09000a;
-        public static final int ip4=0x7f09000b;
-        public static final int port=0x7f09000c;
-        public static final int seekBar1=0x7f090003;
-        public static final int spp_title_logo=0x7f09000e;
+        public static final int action_settings=0x7f090011;
+        public static final int button1=0x7f090006;
+        public static final int button2=0x7f090007;
+        public static final int button3=0x7f090008;
+        public static final int imageView1=0x7f090002;
+        public static final int imageView2=0x7f090003;
+        public static final int ip1=0x7f09000a;
+        public static final int ip2=0x7f09000b;
+        public static final int ip3=0x7f09000c;
+        public static final int ip4=0x7f09000d;
+        public static final int port=0x7f09000e;
+        public static final int seekBar1=0x7f090004;
+        public static final int spp_title_logo=0x7f090010;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090009;
+        public static final int textView4=0x7f090005;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -112,6 +114,9 @@ public final class R {
         public static final int title_screen=0x7f080002;
     }
     public static final class string {
+        public static final int Call=0x7f06000a;
+        public static final int FoldCheck=0x7f060009;
+        public static final int Raise=0x7f06000b;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
