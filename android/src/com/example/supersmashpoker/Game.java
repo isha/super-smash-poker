d11b1afd7d5a8f74ab9060dc27adcd0556f466e1
KCJWagner <kcjwagner@gmail.com>
2013-11-24 16:23:22 -0800

    Cleaned up some code, added some print statements

--- android/src/com/example/supersmashpoker/Game.java
+++ android/src/com/example/supersmashpoker/Game.java
@@ -21,13 +21,10 @@ import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.GestureDetectorCompat;
 import android.util.JsonReader;
 import android.util.JsonWriter;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,10 +60,10 @@ public class Game extends Activity implements SensorEventListener, android.view.
 	private boolean mInitialized;
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
-	private final float NOISE = (float) 2.0;
-	private float mLastX, mLastY, mLastZ;
+	private float mLastX, mLastY;
 	private float time = 0;
 	
+	//Alright folks lets pay some poker
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
@@ -75,22 +72,21 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-		setupActionBar();
         getActionBar().hide();
         
         setWidgetIDs();
 		joinBut.setEnabled(false);
-        betBar.setOnSeekBarChangeListener(new SeekBarListener());
-		
 		setFonts();
 		
+		//Get the player ready
 		player = new Player(0);
 		loadDataFile();
 		
 		TCPReadTimerTask tcp_task = new TCPReadTimerTask();
 		Timer tcp_timer = new Timer();
 		tcp_timer.schedule(tcp_task, 3000, 500);
-		
+
+		//Lets setup some listeners shall we
 		mDetector = new GestureDetectorCompat(this, this);
 		mDetector.setOnDoubleTapListener(this);
 		
@@ -102,16 +98,20 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 		
+        betBar.setOnSeekBarChangeListener(new SeekBarListener());
+		
+        //Set start state
 		enterState(Player.JOIN);
-
 	}
 
+	//Lets not forget some cool fonts
 	private void setFonts(){
 		Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/ssp_game_font.ttf");
 		View root = findViewById(android.R.id.content);
 		applyFont((ViewGroup) root, tf);
 	}
 	
+	//In fact, the font is SO cool, lets recursivly set the font
 	private void applyFont( ViewGroup list, Typeface tf) {
 		for (int i = 0; i < list.getChildCount(); i++) {
             View view = list.getChildAt(i);
@@ -123,6 +123,7 @@ public class Game extends Activity implements SensorEventListener, android.view.
         }
 	}
 	
+	//We got a lot of class variable, we should handle that shit
 	private void setWidgetIDs() {
 		betText = (TextView) findViewById(R.id.BetTextID);
         betBar = (SeekBar) findViewById(R.id.SeekBarID);
@@ -137,26 +138,6 @@ public class Game extends Activity implements SensorEventListener, android.view.
         bankText = (TextView) findViewById(R.id.BankTextID);
         card0 = (ImageView) findViewById(R.id.card0);
         card1 = (ImageView) findViewById(R.id.card1);
-	}
-	
-	private void setupActionBar() {
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.game, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 	//Class which will listen for the seekbar to change and update the betText view accordingly
@@ -321,8 +302,8 @@ public class Game extends Activity implements SensorEventListener, android.view.
 
 	
 	public class TCPReadTimerTask extends TimerTask {
-
 		public int bytes_to_skip = 0;
+		
 		public void run() {
 			SuperSmashPoker app = (SuperSmashPoker) getApplication();
 			if (app.socket != null && app.socket.isConnected() && !app.socket.isClosed()) {
@@ -387,15 +368,14 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		}
 	}
 	
-	//	State Handling
-	
-	//Handles start state
+	//Generic state handling
 	public void enterState(int state){
 		player.state = state;
 		setButtonState();
 		updateAll();
 	}
 	
+	//State handling for dealing cards
 	public void dealtState(int suit1, int rank1, int suit2, int rank2){
 		//Create cards
 		Card[] hand = new Card[2];
@@ -409,13 +389,14 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		enterState(Player.DEALT);
 	}
 	
+	//State handling for when it's the players turn
 	public void actionState(int toCall) {
 		this.toCall = toCall;
 		if(toCall == 0) enterState(Player.LEAD);
 		else if(toCall > 0) enterState(Player.FOLLOW);
 	}
 	
-	//State for when the ends and we need to declare a winner
+	//State handling for when the game ends and we need to declare a winner
 	public void endState(boolean win){
 		if (win)
 			enterState(Player.WIN);
@@ -424,8 +405,8 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		else
 			enterState(Player.LOSE);
 	}
-	
-	// Actions
+
+	//Join button event handler
 	public void joinRequest(View view) {
 		try{
 			sendData(new byte[] {
@@ -443,7 +424,8 @@ public class Game extends Activity implements SensorEventListener, android.view.
 			connectBut.setEnabled(true);
 		}
 	}
-	
+
+	//Connect to DE2 button event handler
 	public void onConnectButton(View view) {
 		saveSettings();
 		openSocket();
@@ -451,6 +433,7 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		connectBut.setEnabled(false);
 	}
 	
+	//Save the IP and Port values
 	public void saveSettings() {
 		SuperSmashPoker app = (SuperSmashPoker) getApplication();
 		
@@ -472,12 +455,14 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		app.ip = addr;
 		app.port = Integer.parseInt(text_port.getText().toString());
 	}
-	
+
+	//Fold/Check button event handler
 	public void foldCheckClicked(View view){
 		enterState(Player.WAITING);
 		sendData(new byte[] {(byte) Player.FOLD});
 	}
-	
+
+	//Call button event handler
 	public void callClicked(View view){
 		if (this.toCall > player.bank) this.toCall = player.bank;
 		player.bank -= this.toCall;
@@ -485,9 +470,9 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		enterState(Player.WAITING);
 		sendData(new byte[] {(byte) Player.CALL});
 	}
-	
+
+	//Raise button event handler
 	public void raiseClicked(View view) {
-		
 		if (toCall > player.bank)
 			toCall = player.bank;
 		int betAmount = toCall + betBar.getProgress();
@@ -523,6 +508,7 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		}
 	}
 	
+	//Updates all widgets
 	public void updateAll(){
 		updateStateView();
 		updateBankView();
@@ -530,13 +516,14 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		updateHandView();
 	}
 	
+	//Gesture handler
 	@Override 
     public boolean onTouchEvent(MotionEvent event){ 
         this.mDetector.onTouchEvent(event);
-        // Be sure to call the superclass implementation
         return super.onTouchEvent(event);
     }
 
+	//Gesture handler
     @Override
     public boolean onFling(MotionEvent event1, MotionEvent event2, 
             float velocityX, float velocityY) {
@@ -549,6 +536,7 @@ public class Game extends Activity implements SensorEventListener, android.view.
         return true;
     }
 
+	//Gesture handler
     @Override
     public boolean onDoubleTap(MotionEvent event) {
     	if (player.state != Player.LEAD){
@@ -560,8 +548,10 @@ public class Game extends Activity implements SensorEventListener, android.view.
         return true;
     }
 
+	//Gesture handler
     @Override
 	public void onSensorChanged(SensorEvent event) {
+    	//It's not even you're turn, you're not allowed to fold
     	if (player.state != Player.FOLLOW){
     		return;
     	}
@@ -577,11 +567,6 @@ public class Game extends Activity implements SensorEventListener, android.view.
 			float deltaX = Math.abs(mLastX - x);
 			float deltaY = Math.abs(mLastY - y);
 			
-			if (deltaX < NOISE)
-				deltaX = (float)0.0;
-			if (deltaY < NOISE)
-				deltaY = (float)0.0;
-			
 			if (deltaX > 3 && deltaY > 3){
 				if (System.nanoTime() - time > 1E9){
 					Log.d("Gesture", "time = " + (System.nanoTime() - time));
@@ -595,16 +580,21 @@ public class Game extends Activity implements SensorEventListener, android.view.
 		}
 	}
     
-	protected void onPause() {
+    //Special handling for pause
+	@Override
+    protected void onPause() {
 		super.onPause();
 		mSensorManager.unregisterListener(this);
 	}
 
+    //Special handling for resume
+	@Override
 	protected void onResume() {
 		super.onResume();
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 	}
-    
+
+    //Special handling for stop
 	@Override
 	public void onStop(){
 		super.onStop();
@@ -651,7 +641,7 @@ public class Game extends Activity implements SensorEventListener, android.view.
     
     
     
-    //Useless crap we'll never use but that we need because we implement gesture listener
+    //Useless crap we'll never use but that we need because we implement stuff
     @Override
     public boolean onDown(MotionEvent event) { 
         return true;
